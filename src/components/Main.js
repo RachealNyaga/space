6a83bfbd5e4f568e0b3bfb50b9d8bc6962091c22
Racheal Nyaga <benracheal552@gmail>
2023-02-22 08:36:29 +0300

    add files

---
+++ src/components/Main.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function Main() {
+  return ( <div className='box'>
+       <div className='box1'>
+      <h4> So, you want to travel to</h4>
+  <h2>SPACE</h2>
+<p>  Let’s face it; if you want to go to space, you might as well genuinely go to 
+  outer space and not hover kind of on the edge of it. Well sit back, and relax 
+  because we’ll give you a truly out of this world experience!</p>
+
+ </div>
+    <div className='box2'> 
+    <p>Explore </p>
+
+    </div>
+  </div>
+ 
+  )
+}
+
+
+export default Main
