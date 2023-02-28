b2382012eaa817c4696549c1d12a403ce62aaf92
RachealNyaga <email@example.com>
2023-02-28 11:19:42 +0300

    space props

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
-import React from 'react'
-import "./components/Style.css"
-import Main from "./components/Main"
-import Nav from "./components/Nav"
+import {useState} from "react"
+
+// import "./components/Style.css"
+import Main from './components/Main'
+import Nav from './components/Nav'
 
 
 function App() {
+const [text,setText] = useState (
+      ` <h4> So, you want to travel to</h4>
+      <h2>SPACE</h2>
+    <p>  Let’s face it; if you want to go to space, you might as well genuinely go to 
+      outer space and not hover kind of on the edge of it. Well sit back, and relax 
+      because we’ll give you a truly out of this world experience!</p>
+    
+     </div>
+        <div className='box2'> 
+        <p>Explore </p>
+    
+        </div>
+      </div>
+     
+      `
+);
+
   return (
     <div className="App">
     
     <Nav/>
-    <Main/>
+    <Main text={text}/>
     </div>
   )
 }
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,23 +1,14 @@
-import React from 'react'
+import './Style.css'
 
-function Main() {
-  return ( <div className='box'>
-       <div className='box1'>
-      <h4> So, you want to travel to</h4>
-  <h2>SPACE</h2>
-<p>  Let’s face it; if you want to go to space, you might as well genuinely go to 
-  outer space and not hover kind of on the edge of it. Well sit back, and relax 
-  because we’ll give you a truly out of this world experience!</p>
-
- </div>
-    <div className='box2'> 
-    <p>Explore </p>
+function Main(props) {
+     console.log (props);
+     
+  return <div className='wrapper'>
+    <div className='box' dangerouslySetInnerHTML = {{_html:props.text}}>
 
     </div>
   </div>
- 
-  )
-}
+ }
 
 
 export default Main
